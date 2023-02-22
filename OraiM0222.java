d3990d427daf76133ffcd3538075287e33ac1252
KSZBence <115688454+KSZBence@users.noreply.github.com>
2023-02-22 12:24:36 +0100

    main hívás

--- OraiM0222.java
+++ OraiM0222.java
@@ -6,9 +6,19 @@ package oraim0222;
  * @author zolko
  */
 public class OraiM0222 {
-
     public static void main(String[] args) {
-        
+        kiir("Az első 10 szám összege: "+elso10SzamOsszege());
+        kiir(1+"+"+2);
+        String szoveg = "\n%d + %d = %d".formatted(1, 2, osszead(1, 2));
+        kiir(szoveg);
+    }
+
+    private static void kiir(String szoveg) {
+        System.out.print(szoveg);
+    }
+
+    private static int osszead(int a, int b) {
+        return a+b;
     }
 
     private static void kiiratas(int ertek) {
