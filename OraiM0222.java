36967bd219401e9ace7710e21ca960e0935ed37b
KSZBence <115688454+KSZBence@users.noreply.github.com>
2023-02-22 12:36:53 +0100

    4 szam összege

--- OraiM0222.java
+++ OraiM0222.java
@@ -9,8 +9,19 @@ public class OraiM0222 {
     public static void main(String[] args) {
         kiir("Az első 10 szám összege: "+elso10SzamOsszege());
         kiir(1+"+"+2);
-        String szoveg = "\n%d + %d = %d".formatted(1, 2, osszead(1, 2));
+        String szoveg = "\n%d + %d = %d\n".formatted(1, 2, osszead(1, 2));
         kiir(szoveg);
+        int[] szamok = {1, 2, 3, 4};
+        szoveg = "A 4 szám összege: %d\n".formatted(Osszeg4Szam(szamok));
+        kiir(szoveg);
+    }
+
+    private static int Osszeg4Szam(int[] szamok) {
+        int osszeg = 0;
+        for (int i = 0; i < szamok.length; i++) {
+            osszeg += szamok[i];
+        }
+        return osszeg;
     }
 
     private static void kiir(String szoveg) {
