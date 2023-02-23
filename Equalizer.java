a66700593980aa5025364b6fe05c1fd4b20c6f4b
KSZBence <115688454+KSZBence@users.noreply.github.com>
2023-02-23 12:56:01 +0100

    mégtöbb túlterhelés

--- Equalizer.java
+++ Equalizer.java
@@ -7,37 +7,49 @@ public class Equalizer {
     static Random rnd = new Random();
     
     public static void main(String[] args) {
-        fel_1_randomEqu();
+//        fel_1_randomEqu();
         fel_2_megadottEq();
     }
 
     private static void fel_2_megadottEq() {
         eq();
+        eq(8, true);
         eq(12);
-        eq();
-        eq(8);
+        eq(true);
         eq();
     }
 
-    private static void fel_1_randomEqu() {
-        for (int i = 0; i < 5; i++) {
-            eq();
-        }
-    }
+//    private static void fel_1_randomEqu() {
+//        for (int i = 0; i < 5; i++) {
+//            eq();
+//        }
+//    }
 
     private static void eq(){
-        int a = rnd.nextInt(7-3+1)+3;
-        for (int i = 0; i < a; i++) {
+        int rndhossz = rnd.nextInt(7-3+1)+3;
+        eq(rndhossz,false);
+    }
+    
+    private static void eq(int hossz){
+        eq(hossz,false);
+    }
+    
+    private static void eq(boolean ir){
+        int rndhossz = rnd.nextInt(7-3+1)+3;
+        eq(rndhossz,ir);
+    }
+    
+    private static void eq(int hossz, boolean ir){
+        for (int i = 0; i < hossz; i++) {
             System.out.print("\u001B[45m" + " ");
+        }
+        if (ir) {
+            System.out.print("\u001B[40m" + " ");
+            System.out.printf("(%d)",hossz);
         }
         System.out.println();
     }
     
-    private static void eq(int hossz){
-        for (int i = 0; i < hossz; i++) {
-            System.out.print("\u001B[45m" + " ");
-        }
-        System.out.println();
-    }
+    
     
 }
